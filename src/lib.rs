c3c4bf345efd402dd844e8e490770bda026ebf02
leohowell <leohowell@leohowell.com>
2019-12-09 01:19:44 +0800

    bump version to 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
+extern crate crypto;
 extern crate regex;
 
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use regex::Regex;
+
+pub mod cryptography;
+
+#[pyclass]
+struct AES {}
+
+#[pymethods]
+impl AES {
+    #[staticmethod]
+    fn encrypt(payload: &str, key: &str, iv: &str) -> String {
+        cryptography::encrypt_to_base64(payload, key, iv)
+    }
+
+    #[staticmethod]
+    fn decrypt(payload: &str, key: &str, iv: &str) -> String {
+        cryptography::decrypt_from_base64(payload, key, iv)
+    }
+}
 
 #[pyclass]
 struct RegexUtil {}
@@ -10,24 +29,23 @@ struct RegexUtil {}
 #[pymethods]
 impl RegexUtil {
     #[staticmethod]
-    fn is_match(regex: &str, content: &str) -> bool {
-        let re = Regex::new(regex).unwrap();
-        re.is_match(content)
+    fn is_match(pattern: &str, content: &str) -> bool {
+        is_match(pattern, content)
     }
 }
 
 #[pyfunction]
-fn is_match(regex: &str, content: &str) -> bool {
-    let re = Regex::new(regex).unwrap();
+fn is_match(pattern: &str, content: &str) -> bool {
+    let re = Regex::new(pattern).unwrap();
     re.is_match(content)
 }
 
-
 #[pymodule]
-fn pyo3_regex(_py: Python, m: &PyModule) -> PyResult<()> {
+fn rustpy_tools(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<RegexUtil>()?;
+    m.add_class::<AES>()?;
     m.add_wrapped(wrap_pyfunction!(is_match))?;
-    m.add("version", "0.1.0")?;
+    m.add("version", "0.2.0")?;
 
     Ok(())
 }
